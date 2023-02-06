09e74df54639418b8e066c9afc9ddff0749ab9d5
Ishmam Bin Rofi <113937167+ishmam-br10@users.noreply.github.com>
2023-02-06 19:06:57 +0600

    Add files via upload

---
+++ ME.java
@@ -0,0 +1,11 @@
+public class ME {
+
+    public static void sayname() {
+        System.out.println("Hello ! I am Ishmam");
+
+    }
+
+    public static void sayage() {
+        System.out.println("I am 21 years old");
+    }
+}
